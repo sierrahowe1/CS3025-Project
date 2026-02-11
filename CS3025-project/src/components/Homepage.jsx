63721c712806f3a509dd69eebfc3479ce3ba9628
sierrahowe1 <sierra.howe@unb.ca>
2026-02-11 09:40:30 -0400

    Updating font sizes

--- CS3025-project/src/components/Homepage.jsx
+++ CS3025-project/src/components/Homepage.jsx
@@ -9,7 +9,7 @@ export default function Homepage({ userName, onLogout}) {
 
 return (
     <div className="min-h-screen w-full bg-gradient-to-br from-gray-50 to-white flex flex-col">
-        <div className="relative w-full bg-gradient-to-br from-cyan-400 via-cyan-300 to-cyan-200 rounded-b-[40px] md:rounded-b-[60px] px-6 py-12 md:py-16 lg:py-20 shadow-lg">
+        <div className="relative w-full bg-gradient-to-br from-cyan-100 via-cyan-300 to-cyan-400 rounded-b-[40px] md:rounded-b-[60px] px-6 py-12 md:py-16 lg:py-20 shadow-lg">
             <div className="flex justify-center mb-8 md:mb-10">
             <img src="src/Image.png" className="w-20 h-20 sm:w-50 sm:h-50 object-contain"/>
 
@@ -22,20 +22,20 @@ return (
                 <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-gray-900 mb-4 md:mb-6">
                     Hello, {userName}!
                 </h1>
-                <p className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-gray-900 mb-4 md:mb-6">What can we do for you today?</p>
+                <p className="text-3xl sm:text-4xl md:text-5xl lg:text-4xl font-bold text-gray-900 mb-4 md:mb-6">What can we do for you today?</p>
             </div>
             </div>
 
             <div className="flex-1 px-4 sm:px-6 md:px-8 lg:px-12 py-8 md:py-12">
                 <div className="max-w-6x1 mx-auto">
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 mb-8">
-                        <button className="bg-gradient-to-br from-cyan-400 to-cyan-300 hover:from-cyan-500 hover:to-cyan-400 text-gray-900 font-semibold text-base md:text-lg px-6 py-6 md:py-8 rounded-[30px] md:rounded-[40px] shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105">Community Board</button>
+                        <button className="bg-gradient-to-br from-cyan-400 to-cyan-300 hover:from-cyan-500 hover:to-cyan-400 text-gray-900 font-semibold text-base md:text-3xl px-6 py-6 md:py-8 rounded-[30px] md:rounded-[40px] shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105">Community Board</button>
 
-                        <button className="bg-gradient-to-br from-cyan-400 to-cyan-300 hover:from-cyan-500 hover:to-cyan-400 text-gray-900 font-semibold text-base md:text-lg px-6 py-6 md:py-8 rounded-[30px] md:rounded-[40px] shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105">Create a Post</button>
+                        <button className="bg-gradient-to-br from-cyan-400 to-cyan-300 hover:from-cyan-500 hover:to-cyan-400 text-gray-900 font-semibold text-base md:text-3xl px-6 py-6 md:py-8 rounded-[30px] md:rounded-[40px] shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105">Create a Post</button>
 
-                        <button className="bg-gradient-to-br from-cyan-400 to-cyan-300 hover:from-cyan-500 hover:to-cyan-400 text-gray-900 font-semibold text-base md:text-lg px-6 py-6 md:py-8 rounded-[30px] md:rounded-[40px] shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105">Account Settings</button>
+                        <button className="bg-gradient-to-br from-cyan-400 to-cyan-300 hover:from-cyan-500 hover:to-cyan-400 text-gray-900 font-semibold text-base md:text-3xl px-6 py-6 md:py-8 rounded-[30px] md:rounded-[40px] shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105">Account Settings</button>
 
-                        <button className="bg-gradient-to-br from-cyan-400 to-cyan-300 hover:from-cyan-500 hover:to-cyan-400 text-gray-900 font-semibold text-base md:text-lg px-6 py-6 md:py-8 rounded-[30px] md:rounded-[40px] shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105" >Messages</button>
+                        <button className="bg-gradient-to-br from-cyan-400 to-cyan-300 hover:from-cyan-500 hover:to-cyan-400 text-gray-900 font-semibold text-base md:text-3xl px-6 py-6 md:py-8 rounded-[30px] md:rounded-[40px] shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105" >Messages</button>
 
                     </div>
 
